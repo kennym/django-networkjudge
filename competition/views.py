c3fd2aa954228f895c4aa488517cdcd7ea214183
Kenny Meyer <knny.myer@gmail.com>
2011-04-29 16:49:41 -0400

    Bug fix: Allow multiple submissions for a participant

--- competition/views.py
+++ competition/views.py
@@ -10,16 +10,15 @@ from django.shortcuts import (
 )
 
 from competition.forms import (
-    UploadSolutionForm
+    UploadSubmissionForm
 )
 from competition.models import (
     Competition,
     Judge,
     Participant,
     Problem,
-    Solution
+    Submission
 )
-from com_judge import ComJudge
 
 
 def index(request):
@@ -70,16 +69,15 @@ def competition_problems(request, id):
 
 
 @login_required
-def competition_solutions(request, competition_id):
+def competition_submissions(request, competition_id):
     competition = get_object_or_404(Competition, pk=competition_id)
-    # Get all solutions of the competition
-    solutions = competition.solution_set.all()
+    submissions = competition.submission_set.all()
 
     context = {
-        "objects": solutions
+        "objects": submissions
     }
 
-    return render_to_response("competition/solution/list.html",
+    return render_to_response("competition/submission/list.html",
                               context,
                               RequestContext(request))
 
@@ -105,7 +103,7 @@ def participant_view(request, id, extra_context=None):
         "competition": competition,
         "team": team,
         "problems": problems,
-        "form": UploadSolutionForm()
+        "form": UploadSubmissionForm()
     }
     if extra_context is not None:
         context.update(extra_context)
@@ -115,48 +113,41 @@ def participant_view(request, id, extra_context=None):
 
 
 @login_required
-def submit_solution(request, problem_id):
+def upload_submission(request, problem_id):
     if request.method == "POST" and request.POST:
-        form = UploadSolutionForm(request.POST)
+        form = UploadSubmissionForm(request.POST)
         if form.is_valid():
             participant = get_object_or_404(Participant, pk=request.user.id)
 
-            # Check if participant has already submitted a solution
-            queryset = Solution.objects.filter(participant__exact=participant.id)
-            if queryset.exists():
-                messages.add_message(request, messages.ERROR, _("You already submitted a solution."))
-                solution = queryset.all()[0]
-            else:
-
-                solution = form.save(commit=False)
-                solution.participant = participant
-                problem = get_object_or_404(Problem, pk=problem_id)
-                solution.competition = participant.competition
-                solution.problem = problem
-                solution.save()
-                solution.compile_and_run()
-            return render_to_response("competition/solution/submit.html",
-                                      {'solution': solution},
+            submission = form.save(commit=False)
+            submission.participant = participant
+            problem = get_object_or_404(Problem, pk=problem_id)
+            submission.competition = participant.competition
+            submission.problem = problem
+            submission.save()
+            submission.compile_and_run()
+            return render_to_response("competition/submission/submit.html",
+                                      {'submission': submission},
                                       context_instance=RequestContext(request))
-        return render_to_response("competition/solution/submit.html",
+        return render_to_response("competition/submission/submit.html",
                                   {'form': form},
                                   context_instance=RequestContext(request))
     else:
         problem = get_object_or_404(Problem, pk=problem_id)
         participant = get_object_or_404(Participant, pk=request.user.id)
-        solution = None
-        for sol in participant.solution_set.all():
+        submission = None
+        for sol in participant.submission_set.all():
             if sol.problem == problem:
-                solution = sol
+                submission = sol
         context = {
-            "form": UploadSolutionForm(),
+            "form": UploadSubmissionForm(),
             "competition": get_object_or_404(Competition, pk=participant.competition.id),
             "problem": problem,
-            "solution": solution
+            "submission": submission
         }
-        return render_to_response("competition/solution/submit.html",
+        return render_to_response("competition/submission/submit.html",
                                   context,
                                   context_instance=RequestContext(request))
 
-def solution_judge(request, judge_id, solution_id):
+def submission_judge(request, judge_id, solution_id):
     pass
